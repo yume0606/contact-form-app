f9deb226647bdf665a10170fba62a1a3ee1f6a47
yume0606 <dreams1524abc@gmail.com>
2026-09-16 21:52:16 +0900

    Webコントローラーの作成とルーティング設定完了

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -1,10 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Tag;
+use App\Http\Requests\StoreTagRequest;
+use App\Http\Requests\UpdateTagRequest;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    public function store(StoreTagRequest $request)
+    {
+        Tag::create($request->validated());
+
+        return redirect('/admin');
+    }
+    public function edit(Tag $tag)
+    {
+        return view('admin.tags.edit', compact('tag'));
+    }
+    public function update(UpdateTagRequest $request, Tag $tag)
+    {
+        $tag->update($request->validated());
+
+        return redirect('/admin');
+    }
+    public function destroy(Tag $tag)
+    {
+        $tag->delete();
+
+        return redirect('/admin');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\TagController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,4 +24,8 @@ Route::middleware('auth')->group(function () {
     Route::get('/admin', [AdminController::class, 'index']);
     Route::get('/admin/contacts/{contact}', [AdminController::class, 'show']);
     Route::delete('/admin/contacts/{contact}', [AdminController::class, 'destroy']);
+    Route::post('/admin/tags', [TagController::class, 'store']);
+    Route::get('/admin/tags/{tag}/edit', [TagController::class, 'edit']);
+    Route::put('/admin/tags/{tag}', [TagController::class, 'update']);
+    Route::delete('/admin/tags/{tag}', [TagController::class, 'destroy']);
 });
